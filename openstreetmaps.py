dfc86f0c5fb0fcc5869b7f43a0134fc69146fab1
sdoggett <50183265+sdoggett@users.noreply.github.com>
2020-01-22 09:14:30 -0800

    testing

--- openstreetmaps.py
+++ openstreetmaps.py
@@ -6,3 +6,43 @@ Created on Tue Jan 21 12:49:57 2020
 """
 
 ##Use Open Street Maps to find grocery stores and transit stops
+
+
+from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
+from OSMPythonTools.nominatim import Nominatim
+
+
+nominatim = Nominatim()
+overpass = Overpass()
+areaId = nominatim.query('Oakland, California').areaId()
+
+
+
+query = overpassQueryBuilder(area=areaId, 
+                             elementType='node', 
+                             selector='"shop"="supermarket"', 
+                             includeGeometry=True,
+                             out='body')
+result = overpass.query(query)
+print('number of supermarkets (nodes):  %s' % result.countElements())
+
+output = {}
+for x in result.elements():
+    nodeID = x.id()
+    shopName = x.tag('name')
+    coord = [x.lat(),x.lon()]
+    
+    output[nodeID] = [shopName, coord]
+    
+for y in output:
+    print(output[y][0])
+    print("")
+    
+#node
+#"shop"="convenience"
+#"shop"="supermarket"
+#amenity
+
+#"highway"="bus_stop"
+#route_ref
+
